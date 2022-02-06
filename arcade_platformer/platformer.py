f00aa82b4d4166efcfc74aedd9ed583fcbd3748d
Mike <mikestewart729@gmail.com>
2022-02-06 15:55:44 -0500

    Create player sprite handling

--- arcade_platformer/platformer.py
+++ arcade_platformer/platformer.py
@@ -33,7 +33,7 @@ ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
 
 class Platformer(arcade.Window):
     def __init__(self):
-        super.__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         # Lists to hold different sets of sprites
         self.coins = None
@@ -122,6 +122,62 @@ class Platformer(arcade.Window):
             gravity_constant=GRAVITY,
             ladders=self.ladders
         )
+    
+    def create_player_sprite(self) -> arcade.AnimatedWalkingSprite:
+        """
+        Creates the animated player sprite
+
+        Returns:
+           The properly set up player sprite
+        """
+        # Path to the textures for the image animation
+        texture_path = ASSETS_PATH / "images" / "player"
+
+        # Set up the appropriate textures
+        walking_paths = [
+            texture_path / f"alienGreen_walk{x}.png" for x in (1, 2)
+        ]
+        climbing_paths = [
+            texture_path / f"alienGreen_climb{x}.png" for x in (1, 2)
+        ]
+        standing_path = texture_path / f"alienGreen_stand.png"
+
+        # Load the textures
+        walking_right_textures = [
+            arcade.load_texture(texture) for texture in walking_paths
+        ]
+        walking_left_textures = [
+            arcade.load_texture(texture, mirrored=True) for texture in walking_paths
+        ]
+        walking_up_textures = [
+            arcade.load_texture(texture) for texture in climbing_paths
+        ]
+        walking_down_textures = [
+            arcade.load_texture(texture, mirrored=True) for texture in climbing_paths
+        ]
+        standing_right_textures = [arcade.load_texture(standing_path)]
+        standing_left_textures = [arcade.load_texture(standing_path, mirrored=True)]
+
+        # Create the sprite
+        player = arcade.AnimatedWalkingSprite()
+
+        # Add the proper textures
+        player.stand_left_textures = standing_left_textures
+        player.stand_right_textures = standing_right_textures
+        player.walk_left_textures = walking_left_textures
+        player.walk_right_textures = walking_right_textures
+        player.walk_up_textures = walking_up_textures
+        player.walk_down_textures = walking_down_textures
+
+        # Set the player defaults
+        player.center_x = PLAYER_START_X
+        player.center_y = PLAYER_START_Y
+        player.state = arcade.FACE_RIGHT
+
+        # Set the initial texture
+        player.texture = player.stand_right_textures[0]
+
+        return player
 
     def on_key_press(self, key: int, modifiers: int):
         """
